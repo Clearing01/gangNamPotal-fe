a1570af110005af6c174157484d3a6859d37b0c9
minho.park <minho.park@twolinecode.com>
2023-01-10 18:15:40 +0900

    first commit

---
+++ src/components/BaseDrawer.vue
@@ -0,0 +1,17 @@
+<template>
+	<q-drawer v-model="drawer" show-if-above side="left" bordered :width="200" behavior="desktop">
+		<q-list class="q-pt-lg" />
+	</q-drawer>
+</template>
+<script setup lang="ts">
+import { useUiStore } from '@/store/ui';
+import { computed } from 'vue';
+
+const uiStore = useUiStore();
+
+const drawer = computed({
+	get: () => uiStore.getDrawer,
+	set: (value: boolean) => uiStore.updateDrawer(value),
+});
+</script>
+<style lang="scss" scoped></style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,16 @@
+<template>
+	<q-layout view="hHh Lpr lFf" class="app-wrapper">
+		<BaseHeader />
+		<BaseDrawer />
+		<q-page-container class="app-container">
+			<router-view />
+		</q-page-container>
+	</q-layout>
+</template>
+
+<script setup lang="ts">
+import BaseHeader from '../components/BaseHeader.vue';
+import BaseDrawer from '../components/BaseDrawer.vue';
+</script>
+
+<style scoped></style>
