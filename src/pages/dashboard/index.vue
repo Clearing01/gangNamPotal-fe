0510d93a727f49a0da7a67d84fa3398ff7d283c5
minho.park <minho.park@twolinecode.com>
2023-01-10 18:33:04 +0900

    초기페이지 및 라우터 설정

---
+++ src/pages/dashboard/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="dashboard-calendar-section">
+        <Calendar/>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import Calendar from '@/components/Calendar.vue';
+
+</script>
+
+<style>
+  .dashboard-calendar-section {
+    padding: 1px 0;
+    height: 100vh;
+  }
+</style>
