6fb622c526ef4699e8f78e311585a15d68e535d7
minho.park <minho.park@twolinecode.com>
2023-01-15 17:24:58 +0900

    출퇴근 현황 페이지 테이블 로우 클릭하여 시간 변경 되도록 수정

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,14 +1,69 @@
 <template>
-	<q-table
-		flat
-		bordered
-		:rows="rows"
-		:columns="columns"
-		row-key="rowNum"
-		:table-class="'common-table'"
-		:no-data-label="'데이터를 찾을 수 없습니다'"
-	>
-	</q-table>
+	<template v-if="propDataSet.isAttendance">
+		<q-table
+			flat
+			bordered
+			:rows="rows"
+			:columns="columns"
+			row-key="rowNum"
+			:table-class="'common-table'"
+			:no-data-label="'데이터를 찾을 수 없습니다'"
+		>
+			<template v-slot:header="props">
+				<q-tr :props="props">
+					<q-th v-for="col in props.cols" :key="col.name" :props="props">
+						{{ col.label }}
+					</q-th>
+				</q-tr>
+			</template>
+
+			<template v-slot:body="props">
+				<q-tr class="cursor-pointer" :props="props">
+					<q-td v-for="col in props.cols" :key="col.name" :props="props" @click="updateModal(true, props)">
+						{{ col.value }}
+					</q-td>
+				</q-tr>
+			</template>
+		</q-table>
+		<q-dialog v-model="commuteUpdateModal" persistent>
+			<q-card>
+				<q-card-section class="row items-center">
+					<p class="q-ml-sm">출퇴근시간 변경</p>
+				</q-card-section>
+				<q-card-section>
+					이름
+					<q-input v-model="employeeData.nameKr" class="app-input input-medium" outlined readonly />
+				</q-card-section>
+				<q-card-section>
+					날짜 <q-input v-model="employeeData.date" class="app-input input-medium" outlined readonly />
+				</q-card-section>
+				<q-card-section>
+					출근시간 <q-input v-model="employeeData.startDt" class="app-input input-medium" outlined placeholder="입력하세요" />
+				</q-card-section>
+				<q-card-section>
+					퇴근시간 <q-input v-model="employeeData.endDt" class="app-input input-medium" outlined placeholder="입력하세요" />
+				</q-card-section>
+
+				<q-card-actions align="right">
+					<q-btn flat label="등록" color="primary" />
+					<q-btn flat label="취소" color="primary" v-close-popup />
+				</q-card-actions>
+			</q-card>
+		</q-dialog>
+	</template>
+
+	<template v-else>
+		<q-table
+			flat
+			bordered
+			:rows="rows"
+			:columns="columns"
+			row-key="rowNum"
+			:table-class="'common-table'"
+			:no-data-label="'데이터를 찾을 수 없습니다'"
+		>
+		</q-table>
+	</template>
 </template>
 
 <script setup>
@@ -19,6 +74,25 @@ const props = defineProps({ tableData: Object });
 const propDataSet = computed(() => props.tableData);
 const columns = propDataSet.value.columnList;
 const rows = propDataSet.value.list;
+
+const commuteUpdateModal = ref(false);
+const employeeData = ref({
+	id: '',
+	nameKr: '',
+	date: '',
+	startDt: '',
+	endDt: '',
+});
+
+const updateModal = (flag, value) => {
+	commuteUpdateModal.value = flag;
+	employeeData.value.nameKr = value.row.nameKr;
+	employeeData.value.date = value.row.date;
+	employeeData.value.startDt = value.row.startDt;
+	employeeData.value.endDt = value.row.endDt;
+
+	console.log(value);
+};
 
 // 적용될 컬럼
 const defaultColumnList = ref([
