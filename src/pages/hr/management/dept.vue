8e2a10993c7e350f7deface6648b8efadf52b2a6
minho.park <minho.park@twolinecode.com>
2023-01-15 17:25:42 +0900

    대시보드, 소속 부서 필터 추가

--- src/pages/hr/management/dept.vue
+++ src/pages/hr/management/dept.vue
@@ -1,17 +1,34 @@
 <template>
-	<Table :tableData="tableDataSet" />
-	<!-- <div class="filter-table-wrapper">
-		<q-table
-		flat
-		bordered
-		:rows="tableDataSet.list"
-		:columns="tableDataSet.columnList"
-		row-key="rowNum"
-	    :table-class="'common-table'"
-		:no-data-label="'데이터를 찾을 수 없습니다'"
-		> 	
-		</q-table>
-	</div> -->
+	<div class="app-pageheader">
+		<span class="main-title">소속/부서</span>
+	</div>
+	<div class="app-page flex no-wrap">
+		<div class="page-left-wrapper">
+			<div class="app-tree-wrapper">
+				<div class="tree-title-section">
+					<div class="tree-title">소속/부서</div>
+				</div>
+				<div class="tree-menu-section">
+					<q-tree :nodes="deptList.list" node-key="label" no-connectors default-expand-all icon="icon-arrow-right">
+						<template v-slot:default-header="prop">
+							<div class="title-wrapper">
+								{{ prop.node.label }}
+								<span class="num-child" v-if="prop.node.childNum >= 0">({{ prop.node.childNum }})</span>
+							</div>
+						</template>
+					</q-tree>
+				</div>
+			</div>
+		</div>
+		<div class="page-right-wrapper">
+			<div class="page-content-header flex justify-between items-center no-wrap">
+				<div class="page-content-title">구성원<span>(사람 수)</span></div>
+				<q-input class="app-input input-medium input-append-search-icon" outlined no-error-icon placeholder="이름을 검색하세요" />
+				<q-btn class="app-btn btn-basic btn-primary" flat @click="employeeUpdate()">입력</q-btn>
+			</div>
+			<Table :tableData="tableDataSet" />
+		</div>
+	</div>
 </template>
 
 <script setup>
@@ -40,6 +57,8 @@ const tableDataSet = ref({
 	], // 테이블에 들어갈 데이터 --> 더미 데이터는 여기에
 	total: 0,
 	isLoading: true,
+	isAttendance: false,
+
 	columnList: [
 		// 테이블 컬럼정보 정의 및 커스텀
 		{ name: 'nameKr', align: 'center', label: '이름', field: 'nameKr', sortable: true },
@@ -54,6 +73,59 @@ const tableDataSet = ref({
 		stat: 'stat',
 	},
 });
+
+const deptList = ref({
+	list: [
+		{
+			label: '구성원',
+			children: [
+				{
+					label: '개발 ',
+					children: [{ label: '개발' }, { label: '퍼블리셔' }],
+				},
+				{
+					label: 'QA ',
+					children: [{ label: 'QA' }],
+				},
+				{
+					label: '경영지원',
+					children: [{ label: '재무' }, { label: '인사총무' }],
+				},
+				{
+					label: '디자인',
+					children: [{ label: 'UI/UX' }],
+				},
+				{
+					label: '마케팅',
+					children: [{ label: 'BX' }],
+				},
+			],
+		},
+	],
+});
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.app-page {
+	min-height: calc(100% - 72px);
+}
+
+.page-right-wrapper {
+	width: calc(100% - 240px);
+}
+
+.page-content-header {
+	margin-bottom: 13px;
+}
+
+.app-input {
+	width: 220px;
+}
+
+.app-table {
+	margin-bottom: 28px;
+}
+.page-content-title {
+	margin-right: 65%;
+}
+</style>
