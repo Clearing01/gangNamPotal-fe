202032676c4ef99ce5798e6fdf9d9ed07de1c337
minho.park <minho.park@twolinecode.com>
2023-01-15 17:18:30 +0900

    출퇴근창 컴포넌트화

---
+++ src/components/CommuteButton.vue
@@ -0,0 +1,68 @@
+<template>
+	<q-dialog v-model="uiStore.start" persistent>
+		<q-card>
+			<img src="../assets/images/teemo.png" alt="" />
+			<q-card-section class="row items-center">
+				<h6 class="q-ml-sm">
+					현재 시각 : {{ uiStore.currentDate }} <br />
+					<span>출근 등록을 하시겠습니까?</span>
+				</h6>
+			</q-card-section>
+
+			<q-card-actions align="right">
+				<q-btn flat label="등록" color="primary" />
+				<q-btn flat label="취소" color="primary" v-close-popup />
+			</q-card-actions>
+		</q-card>
+	</q-dialog>
+
+	<q-dialog v-model="uiStore.end" persistent>
+		<q-card>
+			<img src="../assets/images/teemo.png" alt="" />
+			<q-card-section class="row items-center">
+				<h6 class="q-ml-sm">
+					현재 시각 : {{ uiStore.currentDate }} <br />
+					<span>퇴근 등록을 하시겠습니까?</span>
+				</h6>
+			</q-card-section>
+
+			<q-card-actions align="right">
+				<q-btn flat label="등록" color="primary" />
+				<q-btn flat label="취소" color="primary" v-close-popup />
+			</q-card-actions>
+		</q-card>
+	</q-dialog>
+</template>
+
+<script setup lang="ts">
+import { useUiStore } from '@/store/ui';
+import { log } from 'console';
+import { computed } from 'vue';
+
+const props = defineProps({ currentDate: Object });
+const currentDate = computed(() => props.currentDate);
+const uiStore = useUiStore();
+
+const startShowValue = (flag: boolean) => {
+	uiStore.start = flag;
+	console.log(uiStore.start + ': start');
+};
+
+const endShowValue = (flag: boolean) => {
+	uiStore.end = flag;
+	console.log(uiStore.end + ': end');
+};
+</script>
+
+<style lang="scss">
+.items-center {
+	text-align: center;
+}
+
+.q-card {
+	width: 25%;
+	.q-card-section {
+		text-align: center;
+	}
+}
+</style>
