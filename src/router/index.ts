cbfddc3eb76b354b94f99fe63a0da226a08ae59f
minho.park <minho.park@twolinecode.com>
2023-01-25 17:44:01 +0900

    로그인 안한 상태에서 url 직접 변경 방지

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouterOptions } from 'vue-router';
 import { routes } from './routes';
+import { useAuthStore } from '@/store/auth';
 
 const option: RouterOptions = {
 	scrollBehavior: () => ({ left: 0, top: 0 }),
@@ -10,8 +11,11 @@ const option: RouterOptions = {
 const router = createRouter(option);
 
 router.beforeEach(async (to) => {
-	const hasToken = () => false;
-	if (hasToken()) {
+	const authStore = useAuthStore();
+	const hasToken = authStore.isAuthenticated;
+
+	if (!hasToken && to.path !== '/login' && to.path !== '/beforeEnter') {
+		await router.replace('/login');
 		return true;
 	} else {
 		return true;
